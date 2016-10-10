4bdc4b651a58ab077086aa2d07c114da1d8fe691
mshamshuddeen <shamshuddeen@hotmail.com>
2016-10-10 16:25:38 +0530

    remove radio group listener

--- app/src/main/java/com/dvloper/radiogrouplistener/MainActivity.java
+++ app/src/main/java/com/dvloper/radiogrouplistener/MainActivity.java
@@ -16,14 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rg = (RadioGroup)findViewById(R.id.rg);
-        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                rb = (RadioButton)findViewById(group.getCheckedRadioButtonId());
-
-                Toast.makeText(MainActivity.this, "Checked RadioButton text : "+rb.getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
+       
     }
 }
